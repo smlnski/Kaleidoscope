69aa64de1a05d60eca49b1985d59748c03a4ed19
smlnski <smolinski@posteo.de>
2025-03-19 02:22:09 +0100

    Add Eike's layout for Atreus

--- examples/Devices/Keyboardio/Atreus/Atreus.ino
+++ examples/Devices/Keyboardio/Atreus/Atreus.ino
@@ -34,6 +34,8 @@
 #include "Kaleidoscope-SpaceCadet.h"
 #include "Kaleidoscope-DynamicMacros.h"
 #include "Kaleidoscope-LayerNames.h"
+#include <Kaleidoscope-LongPress.h>
+#include <Kaleidoscope-TapDance.h>
 
 #define MO(n) ShiftToLayer(n)
 #define TG(n) LockLayer(n)
@@ -55,144 +57,142 @@ enum {
 
 enum {
   QWERTY,
-  FUN,
-  UPPER
+  SPC_UMLAUT,
+  ARR_NUM
+};
+
+enum {
+  MACRO_a_UMLAUT,
+  MACRO_A_UMLAUT,
+  MACRO_ESZETT,
+  MACRO_u_UMLAUT,
+  MACRO_U_UMLAUT,
+  MACRO_o_UMLAUT,
+  MACRO_O_UMLAUT
 };
 
 // clang-format off
 KEYMAPS(
   [QWERTY] = KEYMAP_STACKED
   (
-       Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
-      ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
-      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B, Key_Backtick
-      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+    Key_Q   ,Key_W          ,Key_E        ,Key_R      ,Key_T
+    ,Key_A  ,Key_S          ,Key_D        ,Key_F      ,Key_G
+    ,Key_Z  ,Key_X          ,Key_C        ,Key_V      ,Key_B          ,Key_Backtick
+    ,TD(0)  ,Key_LeftShift  ,Key_LeftAlt  ,CTL_T(Tab) ,Key_Backspace  ,TG(ARR_NUM)
 
-                     ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
-                     ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
-       ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_LeftAlt  ,Key_Space ,MO(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
+                      ,Key_Y  ,Key_U       ,Key_I     ,Key_O      ,Key_P
+                      ,Key_H  ,Key_J       ,Key_K     ,Key_L      ,Key_Semicolon
+    ,Key_Backslash    ,Key_N  ,Key_M       ,Key_Comma ,Key_Period ,Key_Slash
+    ,OSL(SPC_UMLAUT)  ,TD(1)  ,Key_LeftGui ,Key_Minus ,Key_Quote  ,TD(2)
   ),
 
-  [FUN] = KEYMAP_STACKED
+  [SPC_UMLAUT] =  KEYMAP_STACKED
   (
-       Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
-      ,Key_LeftParen   ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_RightParen
-      ,Key_LeftBracket ,Key_RightBracket ,Key_Hash      ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
-      ,TG(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Delete         ,Key_LeftControl
+    Key_Exclamation       ,Key_At                 ,Key_Hash         ,Key_Dollar       ,Key_Percent
+    ,M(MACRO_a_UMLAUT)    ,M(MACRO_ESZETT)        ,Key_LeftParen    ,Key_RightParen   ,___
+    ,Key_LeftCurlyBracket ,Key_RightCurlyBracket  ,Key_LeftBracket  ,Key_RightBracket ,___          ,___
+    ,___                  ,___                    ,___              ,___              ,___          ,___
 
-                   ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
-                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,___
-      ,Key_And     ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_Plus
-      ,Key_LeftAlt ,Key_Space    ,___   ,Key_Period ,Key_0 ,Key_Equals
-   ),
+          ,Key_Star   ,M(MACRO_u_UMLAUT) ,___ ,M(MACRO_o_UMLAUT) ,Key_KeypadDivide
+          ,Key_And    ,___               ,___ ,___               ,Key_KeypadSubtract
+    ,___  ,Key_Caret  ,___               ,___ ,___               ,Key_KeypadAdd
+    ,___  ,___        ,___               ,___ ,___               ,Key_Equals
+  ),
 
-  [UPPER] = KEYMAP_STACKED
+  [ARR_NUM] =  KEYMAP_STACKED
   (
-       Key_Insert            ,Key_Home                 ,Key_UpArrow   ,Key_End        ,Key_PageUp
-      ,Key_Delete            ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow ,Key_PageDown
-      ,M(MACRO_VERSION_INFO) ,Consumer_VolumeIncrement ,XXX           ,XXX            ,___ ,___
-      ,MoveToLayer(QWERTY)   ,Consumer_VolumeDecrement ,___           ,___            ,___ ,___
+    ___  ,___           ,Key_UpArrow    ,___            ,___
+    ,___ ,Key_LeftArrow ,Key_DownArrow  ,Key_RightArrow ,___
+    ,___ ,___           ,___            ,___            ,___ ,___
+    ,___ ,___           ,___            ,___            ,___ ,TG(ARR_NUM)
 
-                ,Key_UpArrow   ,Key_F7              ,Key_F8          ,Key_F9         ,Key_F10
-                ,Key_DownArrow ,Key_F4              ,Key_F5          ,Key_F6         ,Key_F11
-      ,___      ,XXX           ,Key_F1              ,Key_F2          ,Key_F3         ,Key_F12
-      ,___      ,___           ,MoveToLayer(QWERTY) ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
-   )
+        ,Key_KeypadMultiply ,Key_7  ,Key_8  ,Key_9      ,Key_KeypadDivide
+        ,___                ,Key_4  ,Key_5  ,Key_6      ,Key_KeypadSubtract
+   ,___ ,___                ,Key_1  ,Key_2  ,Key_3      ,Key_KeypadAdd
+   ,___ ,___                ,___    ,Key_0  ,Key_Period ,Key_Equals
+  )
 )
 // clang-format on
 
 KALEIDOSCOPE_INIT_PLUGINS(
-  // ----------------------------------------------------------------------
-  // Chrysalis plugins
-
-  // The EEPROMSettings & EEPROMKeymap plugins make it possible to have an
-  // editable keymap in EEPROM.
   EEPROMSettings,
   EEPROMKeymap,
-
-  // Focus allows bi-directional communication with the host, and is the
-  // interface through which the keymap in EEPROM can be edited.
   Focus,
-
-  // FocusSettingsCommand adds a few Focus commands, intended to aid in
-  // changing some settings of the keyboard, such as the default layer (via the
-  // `settings.defaultLayer` command)
   FocusSettingsCommand,
-
-  // FocusEEPROMCommand adds a set of Focus commands, which are very helpful in
-  // both debugging, and in backing up one's EEPROM contents.
   FocusEEPROMCommand,
-
-  // The FirmwareVersion plugin lets Chrysalis query the version of the firmware
-  // programmatically.
   FirmwareVersion,
-
-  // The LayerNames plugin allows Chrysalis to display - and edit - custom layer
-  // names, to be shown instead of the default indexes.
   LayerNames,
-
-  // ----------------------------------------------------------------------
-  // Keystroke-handling plugins
-
-  // The Qukeys plugin enables the "Secondary action" functionality in
-  // Chrysalis. Keys with secondary actions will have their primary action
-  // performed when tapped, but the secondary action when held.
   Qukeys,
-
-  // SpaceCadet can turn your shifts into parens on tap, while keeping them as
-  // Shifts when held. SpaceCadetConfig lets Chrysalis configure some aspects of
-  // the plugin.
   SpaceCadet,
   SpaceCadetConfig,
-
-  // Enables the "Sticky" behavior for modifiers, and the "Layer shift when
-  // held" functionality for layer keys.
   OneShot,
   OneShotConfig,
   EscapeOneShot,
   EscapeOneShotConfig,
-
-  // The macros plugin adds support for macros
   Macros,
-
-  // Enables dynamic, Chrysalis-editable macros.
   DynamicMacros,
-
-  // The MouseKeys plugin lets you add keys to your keymap which move the mouse.
-  MouseKeys,
-  MouseKeysConfig  //,
-
-  // The MagicCombo plugin lets you use key combinations to trigger custom
-  // actions - a bit like Macros, but triggered by pressing multiple keys at the
-  // same time.
-  // MagicCombo,
-
-  // Enables the GeminiPR Stenography protocol. Unused by default, but with the
-  // plugin enabled, it becomes configurable - and then usable - via Chrysalis.
-  // GeminiPR,
-);
+  LongPress,
+  LongPressConfig,
+  TapDance);
 
 const macro_t *macroAction(uint8_t macro_id, KeyEvent &event) {
-  if (keyToggledOn(event.state)) {
-    switch (macro_id) {
-    case MACRO_QWERTY:
-      // This macro is currently unused, but is kept around for compatibility
-      // reasons. We used to use it in place of `MoveToLayer(QWERTY)`, but no
-      // longer do. We keep it so that if someone still has the old layout with
-      // the macro in EEPROM, it will keep working after a firmware update.
-      Layer.move(QWERTY);
-      break;
-    case MACRO_VERSION_INFO:
-      Macros.type(PSTR("Keyboardio Atreus - Kaleidoscope "));
-      Macros.type(PSTR(BUILD_INFORMATION));
-      break;
-    default:
-      break;
+  switch (macro_id) {
+  case MACRO_a_UMLAUT:
+    if (keyToggledOn(event.state)) {
+      return MACRO(T(RightAlt), D(LeftShift), T(Quote), U(LeftShift), T(A));
     }
+    break;
+  case MACRO_A_UMLAUT:
+    if (keyToggledOn(event.state)) {
+      return MACRO(T(RightAlt), D(LeftShift), T(Quote), T(A), U(LeftShift));
+    }
+    break;
+  case MACRO_ESZETT:
+    if (keyToggledOn(event.state)) {
+      return MACRO(SEQ(K(RightAlt), K(S), K(S)));
+    }
+    break;
+  case MACRO_u_UMLAUT:
+    if (keyToggledOn(event.state)) {
+      return MACRO(T(RightAlt), D(LeftShift), T(Quote), U(LeftShift), T(U));
+    }
+    break;
+  case MACRO_U_UMLAUT:
+    if (keyToggledOn(event.state)) {
+      return MACRO(T(RightAlt), D(LeftShift), T(Quote), T(U), U(LeftShift));
+    }
+    break;
+  case MACRO_o_UMLAUT:
+    if (keyToggledOn(event.state)) {
+      return MACRO(T(RightAlt), D(LeftShift), T(Quote), U(LeftShift), T(O));
+    }
+    break;
+  case MACRO_O_UMLAUT:
+    if (keyToggledOn(event.state)) {
+      return MACRO(T(RightAlt), D(LeftShift), T(Quote), T(O), U(LeftShift));
+    }
+    break;
+  default:
+    break;
   }
   return MACRO_NONE;
 }
+
+
+void tapDanceAction(uint8_t tap_dance_index, KeyAddr key_addr, uint8_t tap_count, kaleidoscope::plugin::TapDance::ActionType tap_dance_action) {
+  switch (tap_dance_index) {
+  case 0:
+    // Esc/screenshot tapdance
+    return tapDanceActionKeys(tap_count, tap_dance_action, Key_Esc, LSHIFT(LGUI(Key_S)));
+  case 1:
+    // Space/Enter tapdance
+    return tapDanceActionKeys(tap_count, tap_dance_action, Key_Space, Key_Enter);
+  case 2:
+    // Del/Ctrl+Alt+Del tapdance
+    return tapDanceActionKeys(tap_count, tap_dance_action, Key_Delete, LCTRL(LALT(Key_Delete)));
+  }
+}
+
 
 void setup() {
   Kaleidoscope.setup();
@@ -208,6 +208,24 @@ void setup() {
   // can be permanently enabled via Chrysalis, so we should only disable it if
   // no configuration exists.
   SpaceCadetConfig.disableSpaceCadetIfUnconfigured();
+
+  LongPress.enable();
+  LongPress.setTimeout(130);
+  LongPress.setAutoshiftEnabled(LongPress.letterKeys() | LongPress.symbolKeys());
+
+  LONGPRESS(
+    // Window overview on Esc
+    kaleidoscope::plugin::LongPressKey(kaleidoscope::plugin::longpress::ALL_LAYERS, KeyAddr(3, 0), LGUI(Key_Tab)),
+    // Delete last word on Backspace
+    kaleidoscope::plugin::LongPressKey(kaleidoscope::plugin::longpress::ALL_LAYERS, KeyAddr(3, 4), LCTRL(Key_Backspace)),
+    // Delete next word on Delete
+    kaleidoscope::plugin::LongPressKey(kaleidoscope::plugin::longpress::ALL_LAYERS, KeyAddr(3, 11), LCTRL(Key_Delete)),
+    // Autoshift umlauts
+    kaleidoscope::plugin::LongPressKey(SPC_UMLAUT, M(MACRO_a_UMLAUT), M(MACRO_A_UMLAUT)),
+    kaleidoscope::plugin::LongPressKey(SPC_UMLAUT, M(MACRO_u_UMLAUT), M(MACRO_U_UMLAUT)),
+    kaleidoscope::plugin::LongPressKey(SPC_UMLAUT, M(MACRO_o_UMLAUT), M(MACRO_O_UMLAUT)),
+    // F4 on 4
+    kaleidoscope::plugin::LongPressKey(ARR_NUM, Key_4, Key_F4));
 }
 
 void loop() {
