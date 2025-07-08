8f109cb1c87366e5c0c06d0bc7139278075c2c2d
Smolinski, Eike <e.smolinski@weinmann-emt.de>
2025-07-08 08:40:20 +0200

    Use OneShot for SPC_UMLAUT layer

--- examples/Devices/Keyboardio/Atreus/Atreus.ino
+++ examples/Devices/Keyboardio/Atreus/Atreus.ino
@@ -81,7 +81,7 @@ KEYMAPS(
     Key_Q   ,Key_W            ,Key_E        ,Key_R      ,Key_T
     ,Key_A  ,Key_S            ,Key_D        ,Key_F      ,Key_G
     ,Key_Z  ,Key_X            ,Key_C        ,Key_V      ,Key_B        ,Key_Backtick
-    ,TD(0)  ,SFT_T(CapsLock)  ,Key_LeftAlt  ,CTL_T(Tab) ,TG(ARR_NUM)  ,SH(SPC_UMLAUT)
+    ,TD(0)  ,SFT_T(CapsLock)  ,Key_LeftAlt  ,CTL_T(Tab) ,TG(ARR_NUM)  ,OSL(SPC_UMLAUT)
 
                       ,Key_Y  ,Key_U       ,Key_I     ,Key_O      ,Key_P
                       ,Key_H  ,Key_J       ,Key_K     ,Key_L      ,Key_Semicolon
