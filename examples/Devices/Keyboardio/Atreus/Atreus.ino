1321bebc40feec6aa6e406b5487aa3a2f1278abe
Smolinski, Eike <e.smolinski@weinmann-emt.de>
2025-07-18 13:36:10 +0200

    Remove tap dance for ctrl+alt+delete

--- examples/Devices/Keyboardio/Atreus/Atreus.ino
+++ examples/Devices/Keyboardio/Atreus/Atreus.ino
@@ -86,7 +86,7 @@ KEYMAPS(
                       ,Key_Y  ,Key_U       ,Key_I     ,Key_O      ,Key_P
                       ,Key_H  ,Key_J       ,Key_K     ,Key_L      ,Key_Semicolon
     ,Key_Backslash    ,Key_N  ,Key_M       ,Key_Comma ,Key_Period ,Key_Slash
-    ,Key_Backspace    ,TD(1)  ,Key_LeftGui ,Key_Minus ,Key_Quote  ,TD(2)
+    ,Key_Backspace    ,TD(1)  ,Key_LeftGui ,Key_Minus ,Key_Quote  ,Key_Delete
   ),
 
   [SPC_UMLAUT] =  KEYMAP_STACKED
@@ -200,9 +200,6 @@ void tapDanceAction(uint8_t tap_dance_index, KeyAddr key_addr, uint8_t tap_count
   case 1:
     // Space/Enter tapdance
     return tapDanceActionKeys(tap_count, tap_dance_action, Key_Space, Key_Enter);
-  case 2:
-    // Del/Ctrl+Alt+Del tapdance
-    return tapDanceActionKeys(tap_count, tap_dance_action, Key_Delete, LCTRL(LALT(Key_Delete)));
   }
 }
 
